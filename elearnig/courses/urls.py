1e8afbb0c182185974fad6d8ae364bb1d73a242c
root <root@localhost.localdomain>
2021-11-06 00:37:19 +0100

    Secure data

--- elearnig/courses/urls.py
+++ elearnig/courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('/', views.home, name='courses-home'),
+	path('', views.home, name='courses-home'),
 	path('reports/', views.report, name='courses-reports'),
 ]
--- elearnig/elearnig/settings.py
+++ elearnig/elearnig/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&feap4-+@49ya6^2qk1sj8cp2ll)x=-#qq$)l8$bzi!41jia7y'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,13 +76,14 @@ WSGI_APPLICATION = 'elearnig.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+PUSER = config['PUSER']
+PPASSWORD = config['PPASSWORD']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ledb',
-        'USER': 'leuser',
-        'PASSWORD': 'Z^Ti3LTZkqiD',
+        'USER': PUSER,
+        'PASSWORD': PPASSWORD,
 	'HOST': 'localhost',
         'PORT': '',
     }
