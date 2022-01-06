62a9630e458d6d2e713d2d008e78fa91c5571f75
mjuc <michal.jucewicz@protonmail.com>
2022-01-06 14:08:41 +0100

    Reverted tested fixes - none worked.

--- elearnig/auth/models.py
+++ elearnig/auth/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
-class User(User):
+class User(AbstractUser):
     username = models.CharField(blank=True, null=True, max_length=50)
     email = models.EmailField(_('email address'), unique=True)
 
