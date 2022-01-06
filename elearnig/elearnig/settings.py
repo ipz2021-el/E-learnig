e5ab04f159a2dad764072b1cc891245571653fcd
mjuc <michal.jucewicz@protonmail.com>
2022-01-06 14:38:29 +0100

    Reverted changes from previous commit

--- elearnig/elearnig/settings.py
+++ elearnig/elearnig/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_otp',
     'django_otp.plugins.otp_totp',
     'rest_auth',
