1f644871631a2355ab8732c8370c4576f0abd151
mjuc <michal.jucewicz@protonmail.com>
2022-01-12 12:00:00 +0100

    Fixed AUTH_USER_MODEL issue.

--- elearnig/elearnig/settings.py
+++ elearnig/elearnig/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'userapi.User'
+AUTH_USER_MODEL = 'auth.User'
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
