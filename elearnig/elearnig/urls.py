ba09909d33b2a1a06d520147ba095f535802ad25
mjuc <michal.jucewicz@protonmail.com>
2022-01-15 10:34:01 +0100

    Modified includes in urls.py

--- elearnig/elearnig/urls.py
+++ elearnig/elearnig/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authorize import urls
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
@@ -22,7 +23,7 @@ from rest_framework_jwt.views import verify_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('courses.urls')),
-    path('users/', include('authorize.urls')),
+    path('users/', include(urls)),
 #    path(r'^settings/', include('django_mfa.urls')),
     path(r'api-token-auth/', obtain_jwt_token),
     path(r'api-token-refresh/', refresh_jwt_token),
