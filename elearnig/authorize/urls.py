bd06706d0a08c2e443147f47685fae6f7c37a932
mjuc <michal.jucewicz@protonmail.com>
2022-01-20 22:37:02 +0100

    Modified otp urls.

--- elearnig/authorize/urls.py
+++ elearnig/authorize/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 
 urlpatterns = [
     #other paths
-    path('totp/create/', views.TOTPCreateView.as_view(), name='totp-create'),
-    path('totp/login/(?P<token>[0-9]{6})/$', views.TOTPVerifyView.as_view(), name='totp-login'),
+    path('totp/create/', views.TOTPCreateView.as_view()),
+    path('totp/login/(?P<token>[0-9]{6})/$', views.TOTPVerifyView.as_view()),
     path('', views.UserListView.as_view()),
     path('rest-auth/', include('rest_auth.urls')),
 ]
