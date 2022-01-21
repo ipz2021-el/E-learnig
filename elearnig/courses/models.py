9d23cea7db0c0854693870de87582f5935af6009
mjuc <michal.jucewicz@protonmail.com>
2022-01-21 17:13:51 +0100

    Updated data models.

--- elearnig/courses/models.py
+++ elearnig/courses/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.conf import settings
 
 class Subject(models.Model):
@@ -66,3 +67,13 @@ class Image(ItemBase):
  
 class Video(ItemBase):
     url = models.URLField()
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents')
+    content_type = content_type = models.ForeignKey(ContentType,
+                   limit_choices_to={'model__in':('text',
+                                        'video',
+                                        'image',
+                                        'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
