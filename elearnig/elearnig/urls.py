bb5c01012e08c579ffa492d58a6c233d82b58c9d
mjuc <michal.jucewicz@protonmail.com>
2022-01-22 00:01:28 +0100

    Removed unused views.

--- elearnig/elearnig/urls.py
+++ elearnig/elearnig/urls.py
@@ -15,9 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
