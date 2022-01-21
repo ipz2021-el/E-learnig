b104e2928e17f60640eaefade7a6f885009885f6
mjuc <michal.jucewicz@protonmail.com>
2022-01-21 18:01:03 +0100

    Fixed return value in __str__ method in Module class.

--- elearnig/courses/models.py
+++ elearnig/courses/models.py
@@ -45,7 +45,7 @@ class Module(models.Model):
         ordering = ['order']
 
     def __str__(self):
-        return f'{self.order}. {self.title}'
+        return '{}. {}'.format(self.order, self.title)
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, 
