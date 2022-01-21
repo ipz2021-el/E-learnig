38bb3ec19f087773d2233347e245632bc7f80766
mjuc <michal.jucewicz@protonmail.com>
2022-01-21 17:56:32 +0100

    Added ordering to courses data model.

--- elearnig/courses/models.py
+++ elearnig/courses/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.conf import settings
+from .fields import OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -38,9 +39,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
- 
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, 
@@ -76,4 +81,8 @@ class Content(models.Model):
                                         'file')},on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
     
