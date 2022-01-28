d879ec84b52d19238669be8749449c1bf0abd236
mjuc <michal.jucewicz@protonmail.com>
2022-01-28 14:24:17 +0100

    Fixed urls.

--- elearnig/courses/urls.py
+++ elearnig/courses/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, url
+from django import path
 from . import views
 
-urlpatterns = [
+pathpatterns = [
 	path('', views.home, name='courses-home'),
 	path('reports/', views.report, name='courses-reports'),
-	url(r'^api/tutorials$', views.course_list),
-	url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.course_detail),
+	path(r'^api/tutorials$', views.course_list),
+	path(r'^api/tutorials/(?P<pk>[0-9]+)$', views.course_detail),
 ]
