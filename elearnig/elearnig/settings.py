e9662f2436ed86d5168a612d527796311745f0f0
Milena Michalska <milena.michalska@dac.digital>
2022-02-02 20:02:09 +0100

    django quickfixes from my testing session

--- elearnig/elearnig/settings.py
+++ elearnig/elearnig/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,7 +91,7 @@ PPASSWORD = config['PPASSWORD']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': 'ledb',
         # 'USER': PUSER,
--- elearnig/manage.py
+++ elearnig/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django
 
 
 def main():
@@ -15,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    django.setup()
 
 
 if __name__ == '__main__':
