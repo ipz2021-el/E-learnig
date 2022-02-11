0e8bda725a07b7cf904f827e06fee2ec809f71d9
mjuc <michal.jucewicz@protonmail.com>
2022-02-11 22:19:18 +0100

    urls fix

--- elearnig/courses/urls.py
+++ elearnig/courses/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 	path('', views.home, name='courses-home'),
 	path('reports/', views.report, name='courses-reports'),
 	path(r'^api/courses$', views.course_list),
-	path(r'^api/courses/(?P<pk>[0-9]+)$', views.course_detail),
+	path(r'^api/courses/(?P<pk>)', views.course_detail),
 	path(r'^api/quizes$', views.quiz_list),
-	path(r'^api/quizes/(?P<pk>[0-9]+)$', views.quiz_detail),
+	path(r'^api/quizes/(?P<pk>)', views.quiz_detail),
 ]
